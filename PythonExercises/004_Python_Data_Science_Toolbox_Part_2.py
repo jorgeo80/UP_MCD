c715dff0e218bf282bcdf68917734cce555b6e7a
Jorge Octavio <jorgeo80@gmail.com>
2020-02-12 11:53:42 -0600

    :monkey: DS-ToolBox-2

--- PythonExercises/004_Python_Data_Science_Toolbox_Part_2.py
+++ PythonExercises/004_Python_Data_Science_Toolbox_Part_2.py
@@ -4,37 +4,40 @@
 #################################################################
 #################################################################
 
-###########################
-####   Ejercicio 1    #####
-###########################
+#================= Exercise 1
 # Create a list of strings: flash
 flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
+
 # Print each list item in flash using a for loop
 for person in flash:
     print(person)
-# Cnreate an iterator for flash: superspeed
-for superspeed in flash:
-    superspeed=iter(superspeed)
-# Print each item from the iterator
-print(next(superspeed))
-print(next(superspeed))
-print(next(superspeed))
-print(next(superspeed))
 
-###########################
-####   Ejercicio 2    #####
-###########################
+
+# Create an iterator for flash: superhero
+superhero = iter(flash)
+
+# Print each item from the iterator
+print(next(superhero))
+print(next(superhero))
+print(next(superhero))
+print(next(superhero))
+
+#================= Exercise 2
 # Create an iterator for range(3): small_value
 small_value = iter(range(3))
+
 # Print the values in small_value
 print(next(small_value))
 print(next(small_value))
 print(next(small_value))
+
 # Loop over range(3) and print the values
 for num in range(3):
     print(num)
+    
 # Create an iterator for range(10 ** 100): googol
 googol= iter(range(10 ** 100))
+
 # Print the first 5 values from googol
 print(next(googol))
 print(next(googol))
@@ -42,100 +45,106 @@ print(next(googol))
 print(next(googol))
 print(next(googol))
 
-###########################
-####   Ejercicio 3    #####
-###########################
+#================= Exercise 3
 # Create a range object: values
 values=range(10,21)
+
 # Print the range object
 print(values)
+
 # Create a list of integers: values_list
 values_list=list(values)
+
 # Print values_list
 print(values_list)
+
 # Get the sum of values: values_sum
 values_sum =sum(values)
+
 # Print values_sum
 print(values_sum)
 
-###########################
-####   Ejercicio 4    #####
-###########################
+#================= Exercise 4
 # Create a list of strings: mutants
 mutants = ['charles xavier', 
-            'bobby drake', 
-            'kurt wagner', 
-            'max eisenhardt', 
-            'kitty pryde']
+           'bobby drake', 
+           'kurt wagner', 
+           'max eisenhardt', 
+           'kitty pryde']
+           
 # Create a list of tuples: mutant_list
 mutant_list = list(enumerate(mutants))
+
 # Print the list of tuples
 print(mutant_list)
+
 # Unpack and print the tuple pairs
 for index1,value1 in enumerate(mutants):
     print(index1, value1)
+    
 # Change the start index
 for index2,value2 in enumerate(mutants,start=1):
     print(index2, value2)
 
-###########################
-####   Ejercicio 5    #####
-###########################
+#================= Exercise 5
 # Create a list of tuples: mutant_data
 mutant_data = list(zip(mutants, aliases, powers))
+
 # Print the list of tuples
 print(mutant_data)
+
 # Create a zip object using the three lists: mutant_zip
 mutant_zip = zip(mutants, aliases, powers)
+
 # Print the zip object
 print(mutant_zip)
+
 # Unpack the zip object and print the tuple values
 for value1, value2, value3 in mutant_zip:
     print(value1, value2, value3)
 
-###########################
-####   Ejercicio 6    #####
-###########################
+#================= Exercise 6
 # Create a zip object from mutants and powers: z1
-z1 =zip(mutants,powers)
+z1 = zip(mutants, powers)
+
 # Print the tuples in z1 by unpacking with *
 print(*z1)
+
 # Re-create a zip object from mutants and powers: z1
-z1=zip(mutants,powers)
+z1 = zip(mutants, powers)
+
 # 'Unzip' the tuples in z1 by unpacking with * and zip(): result1, result2
 result1, result2 = zip(*z1)
+
 # Check if unpacked tuples are equivalent to original tuples
 print(result1 == mutants)
 print(result2 == powers)
 
-###########################
-####   Ejercicio 7    #####
-###########################
+#================= Exercise 7
 # Initialize an empty dictionary: counts_dict
-counts_dict={}
+counts_dict = {}
+
 # Iterate over the file chunk by chunk
-for chunk in pd.read_csv('tweets.csv',chunksize=10):
+for chunk in pd.read_csv('tweets.csv', chunksize = 10):
     # Iterate over the column in DataFrame
     for entry in chunk['lang']:
         if entry in counts_dict.keys():
             counts_dict[entry] += 1
         else:
             counts_dict[entry] = 1
+            
 # Print the populated dictionary
 print(counts_dict)
 
-
-###########################
-####   Ejercicio 8    #####
-###########################
+#================= Exercise 8
 # Define count_entries()
-def count_entries(csv_file,c_size,colname):
+def count_entries(csv_file, c_size, colname):
     """Return a dictionary with counts of
     occurrences as value for each key."""
     # Initialize an empty dictionary: counts_dict
     counts_dict = {}
 # Iterate over the file chunk by chunk
-    for chunk in pd.read_csv(csv_file,chunksize=c_size):
+    for chunk in pd.read_csv(csv_file, chunksize = c_size):
         # Iterate over the column in DataFrame
         for entry in chunk[colname]:
             if entry in counts_dict.keys():
@@ -144,97 +153,156 @@ def count_entries(csv_file,c_size,colname):
                 counts_dict[entry] = 1
     # Return counts_dict
     return counts_dict
+    
 # Call count_entries(): result_counts
-result_counts = count_entries('tweets.csv',10,'lang')
+result_counts = count_entries('tweets.csv', 10, 'lang')
+
 # Print result_counts
 print(result_counts)
 
-###########################
-####   Ejercicio 9    #####
-###########################
 
+#================= Exercise 9
+#list comprehension example [doc[0] for doc in doctor]
 
-###########################
-####   Ejercicio 10   #####
-###########################
+# Create list comprehension: squares
+squares = [i ** 2 for i in range(10)]
 
-###########################
-####   Ejercicio 11   #####
-###########################
+#================= Exercise 10
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
 
-###########################
-####   Ejercicio 12   #####
-###########################
+# Print the matrix
+for row in matrix:
+    print(row)
 
-###########################
-####   Ejercicio 13   #####
-###########################
+#================= Exercise 11
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
 
-###########################
-####   Ejercicio 14   #####
-###########################
+# Create list comprehension: new_fellowship
+new_fellowship = [member for member in fellowship if len(member) >= 7]
 
-###########################
-####   Ejercicio 15   #####
-###########################
+# Print the new list
+print(new_fellowship)
 
-###########################
-####   Ejercicio 16   #####
-###########################
+#================= Exercise 12
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
 
-###########################
-####   Ejercicio 17   #####
-###########################
+# Create list comprehension: new_fellowship
+new_fellowship = [member if len(member) >= 7 else '' for member in fellowship]
 
-###########################
-####   Ejercicio 18   #####
-###########################
+# Print the new list
+print(new_fellowship)
 
-###########################
-####   Ejercicio 19   #####
-###########################
+#================= Exercise 13
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
 
-###########################
-####   Ejercicio 20   #####
-###########################
+# Create dict comprehension: new_fellowship
+new_fellowship = {member: len(member) for member in fellowship}
 
-###########################
-####   Ejercicio 21   #####
-###########################
+# Print the new list
+print(new_fellowship)
 
-###########################
-####   Ejercicio 22   #####
-###########################
+#================= Exercise 14
+# Create generator object: result
+result = (num for num in range(31))
 
-###########################
-####   Ejercicio 23   #####
-###########################
+# Print the first 5 values
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
 
-###########################
-####   Ejercicio 24   #####
-###########################
+# Print the rest of the values
+for value in result:
+    print(value)
 
-###########################
-####   Ejercicio 25   #####
-###########################
+#================= Exercise 15
+# Create a list of strings: lannister
+lannister = ['cersei', 'jaime', 'tywin', 'tyrion', 'joffrey']
 
-###########################
-####   Ejercicio 26   #####
-###########################
+# Create a generator object: lengths
+lengths = (len(person) for person in lannister)
 
-###########################
-####   Ejercicio 27   #####
-###########################
+# Iterate over and print the values in lengths
+for value in lengths:
+    print(value)
 
-###########################
-####   Ejercicio 28   #####
-###########################
+#================= Exercise 16
+# Create a list of strings
+lannister = ['cersei', 'jaime', 'tywin', 'tyrion', 'joffrey']
 
-###########################
-####   Ejercicio 29   #####
-###########################
+# Define generator function get_lengths
+def get_lengths(input_list):
+    """Generator function that yields the
+    length of the strings in input_list."""
+    # Yield the length of a string
+    for person in input_list:
+        yield len(person)
 
-###########################
-####   Ejercicio 30   #####
-###########################
+# Print the values generated by get_lengths()
+for value in get_lengths(lannister):
+    print(value)
 
+#================= Exercise 17
+# Extract the created_at column from df: tweet_time
+tweet_time = df.created_at
+
+# Extract the clock time: tweet_clock_time
+tweet_clock_time = [entry[11:19] for entry in tweet_time]
+
+# Print the extracted times
+print(tweet_clock_time)
+
+#================= Exercise 18
+# Extract the created_at column from df: tweet_time
+tweet_time = df.created_at
+
+# Extract the clock time: tweet_clock_time
+tweet_clock_time = [entry[11:19] for entry in tweet_time if entry[17:19] == '19']
+
+# Print the extracted times
+print(tweet_clock_time)
+
+#================= Exercise 19
+# Zip lists: zipped_lists
+zipped_lists = zip(feature_names, row_vals)
+
+# Create a dictionary: rs_dict
+rs_dict = dict(zipped_lists)
+
+# Print the dictionary
+print(rs_dict)
+
+#================= Exercise 20
+# Define lists2dict()
+def lists2dict(list1, list2):
+    """Return a dictionary where list1 provides
+    the keys and list2 provides the values."""
+    # Zip lists: zipped_lists
+    zipped_lists = zip(list1, list2)
+    # Create a dictionary: rs_dict
+    rs_dict = dict(zipped_lists)
+    # Return the dictionary
+    return rs_dict
+
+# Call lists2dict: rs_fxn
+rs_fxn = lists2dict(feature_names, row_vals)
+
+# Print rs_fxn
+print(rs_fxn)
+
+#================= Exercise 21
+# Print the first two lists in row_lists
+print(row_lists[0])
+print(row_lists[1])
+
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names,sublist) for sublist in row_lists]
+
+# Print the first two dictionaries in list_of_dicts
+print(list_of_dicts[0])
+print(list_of_dicts[1])
