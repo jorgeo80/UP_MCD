0f40f147b5bb1112b63e7bee561ff9c6bebe6bb5
Jorge Octavio <jorgeo80@gmail.com>
2020-02-19 16:33:37 -0600

    :dog: Curso Machine Learning

--- PythonExercises/ML/001_Supervised_Learning_scikit-learn.py
+++ PythonExercises/ML/001_Supervised_Learning_scikit-learn.py
@@ -200,3 +200,447 @@ print(np.mean(cvscores_3))
 cvscores_10 = cross_val_score(reg, X, y, cv = 10)
 print(np.mean(cvscores_10))
 
+# ===================== #
+# ==== Ejercicio 11 === #
+# ===================== #
+
+# Import Lasso
+from sklearn.linear_model import Lasso
+# Instantiate a lasso regressor: lasso
+lasso = Lasso(alpha = 0.4, normalize = True)
+# Fit the regressor to the data
+lasso.fit(X, y)
+# Compute and print the coefficients
+lasso_coef = lasso.coef_
+print(lasso_coef)
+# Plot the coefficients
+plt.plot(range(len(df_columns)), lasso_coef)
+plt.xticks(range(len(df_columns)), df_columns.values, rotation=60)
+plt.margins(0.02)
+plt.show()
+
+# ===================== #
+# ==== Ejercicio 12 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+# Setup the array of alphas and lists to store scores
+alpha_space = np.logspace(-4, 0, 50)
+ridge_scores = []
+ridge_scores_std = []
+# Create a ridge regressor: ridge
+ridge = Ridge(normalize = True)
+
+# Compute scores over range of alphas
+for alpha in alpha_space:
+    # Specify the alpha value to use: ridge.alpha
+    ridge.alpha = alpha
+    # Perform 10-fold CV: ridge_cv_scores
+    ridge_cv_scores = cross_val_score(ridge, X, y, cv = 10)
+    # Append the mean of ridge_cv_scores to ridge_scores
+    ridge_scores.append(np.mean(ridge_cv_scores))
+    # Append the std of ridge_cv_scores to ridge_scores_std
+    ridge_scores_std.append(np.std(ridge_cv_scores))
+# Display the plot
+display_plot(ridge_scores, ridge_scores_std)
+
+# ===================== #
+# ==== Ejercicio 13 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+# Create training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
+# Instantiate a k-NN classifier: knn
+knn = KNeighborsClassifier(n_neighbors = 6)
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+# Predict the labels of the test data: y_pred
+y_pred = knn.predict(X_test)
+# Generate the confusion matrix and classification report
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+# ===================== #
+# ==== Ejercicio 14 === #
+# ===================== #
+
+# Import the necessary modules
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=42)
+# Create the classifier: logreg
+logreg = LogisticRegression()
+# Fit the classifier to the training data
+logreg.fit(X_train, y_train)
+# Predict the labels of the test set: y_pred
+y_pred = logreg.predict(X_test)
+# Compute and print the confusion matrix and classification report
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+# ===================== #
+# ==== Ejercicio 15 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.metrics import roc_curve
+# Compute predicted probabilities: y_pred_prob
+y_pred_prob = logreg.predict_proba(X_test)[:,1]
+# Generate ROC curve values: fpr, tpr, thresholds
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+# Plot ROC curve
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+
+# ===================== #
+# ==== Ejercicio 16 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score
+# Compute predicted probabilities: y_pred_prob
+y_pred_prob = logreg.predict_proba(X_test)[:,1]
+# Compute and print AUC score
+print("AUC: {}".format(roc_auc_score(y_test, y_pred_prob)))
+# Compute cross-validated AUC scores: cv_auc
+cv_auc = cross_val_score(logreg, X, y, cv = 5, scoring = 'roc_auc')
+# Print list of AUC scores
+print("AUC scores computed using 5-fold cross-validation: {}".format(cv_auc))
+
+# ===================== #
+# ==== Ejercicio 17 === #
+# ===================== #
+
+
+from sklearn.metrics import roc_curve
+# Compute predicted probabilities: y_pred_prob
+y_pred_prob = logreg.predict_proba(X_test)[:,1]
+# Generate ROC curve values: fpr, tpr, thresholds
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+# Plot ROC curve
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+
+# ===================== #
+# ==== Ejercicio 16 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score
+# Compute predicted probabilities: y_pred_prob
+y_pred_prob = logreg.predict_proba(X_test)[:,1]
+# Compute and print AUC score
+print("AUC: {}".format(roc_auc_score(y_test, y_pred_prob)))
+# Compute cross-validated AUC scores: cv_auc
+cv_auc = cross_val_score(logreg, X, y, cv = 5, scoring = 'roc_auc')
+# Print list of AUC scores
+print("AUC scores computed using 5-fold cross-validation: {}".format(cv_auc))
+
+# ===================== #
+# ==== Ejercicio 17 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+# Setup the hyperparameter grid
+c_space = np.logspace(-5, 8, 15)
+param_grid = {'C': c_space}
+# Instantiate a logistic regression classifier: logreg
+logreg = LogisticRegression()
+# Instantiate the GridSearchCV object: logreg_cv
+logreg_cv = GridSearchCV(logreg, param_grid, cv = 5)
+# Fit it to the data
+logreg_cv.fit(X,y)
+# Print the tuned parameters and score
+print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_)) 
+print("Best score is {}".format(logreg_cv.best_score_))
+
+# ===================== #
+# ==== Ejercicio 18 === #
+# ===================== #
+
+# Import necessary modules
+from scipy.stats import randint
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import RandomizedSearchCV
+# Setup the parameters and distributions to sample from: param_dist
+param_dist = {"max_depth": [3, None],
+              "max_features": randint(1, 9),
+              "min_samples_leaf": randint(1, 9),
+              "criterion": ["gini", "entropy"]}
+
+# Instantiate a Decision Tree classifier: tree
+tree = DecisionTreeClassifier()
+# Instantiate the RandomizedSearchCV object: tree_cv
+tree_cv = RandomizedSearchCV(tree, param_dist, cv = 5)
+# Fit it to the data
+tree_cv.fit(X, y)
+# Print the tuned parameters and score
+print("Tuned Decision Tree Parameters: {}".format(tree_cv.best_params_))
+print("Best score is {}".format(tree_cv.best_score_))
+
+# ===================== #
+# ==== Ejercicio 19 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+# Create the hyperparameter grid
+c_space = np.logspace(-5, 8, 15)
+param_grid = {'C': c_space, 'penalty': ['l1', 'l2']}
+# Instantiate the logistic regression classifier: logreg
+logreg = LogisticRegression()
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
+# Instantiate the GridSearchCV object: logreg_cv
+logreg_cv = GridSearchCV(logreg, param_grid, cv = 5)
+# Fit it to the training data
+logreg_cv.fit(X_train, y_train)
+# Print the optimal parameters and best score
+print("Tuned Logistic Regression Parameter: {}".format(logreg_cv.best_params_))
+print("Tuned Logistic Regression Accuracy: {}".format(logreg_cv.best_score_))
+
+# ===================== #
+# ==== Ejercicio 20 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
+# Create the hyperparameter grid
+l1_space = np.linspace(0, 1, 30)
+param_grid = {'l1_ratio': l1_space}
+# Instantiate the ElasticNet regressor: elastic_net
+elastic_net = ElasticNet()
+# Setup the GridSearchCV object: gm_cv
+gm_cv = GridSearchCV(elastic_net, param_grid, cv = 5)
+# Fit it to the training data
+gm_cv.fit(X_train, y_train)
+# Predict on the test set and compute metrics
+y_pred = gm_cv.predict(X_test)
+r2 = gm_cv.score(X_test, y_test)
+mse = mean_squared_error(y_test, y_pred)
+print("Tuned ElasticNet l1 ratio: {}".format(gm_cv.best_params_))
+print("Tuned ElasticNet R squared: {}".format(r2))
+print("Tuned ElasticNet MSE: {}".format(mse))
+
+# ===================== #
+# ==== Ejercicio 21 === #
+# ===================== #
+
+# Import pandas
+import pandas as pd
+# Read 'gapminder.csv' into a DataFrame: df
+df = pd.read_csv('gapminder.csv', sep = ',')
+# Create a boxplot of life expectancy per region
+df.boxplot('life', 'Region', rot=60)
+# Show the plot
+plt.show()
+
+# ===================== #
+# ==== Ejercicio 22 === #
+# ===================== #
+
+# Create dummy variables: df_region
+df_region = pd.get_dummies(df)
+# Print the columns of df_region
+print(df_region.columns)
+# Create dummy variables with drop_first=True: df_region
+df_region = pd.get_dummies(df, drop_first = True)
+# Print the new columns of df_region
+print(df_region.columns)
+
+# ===================== #
+# ==== Ejercicio 23 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+# Instantiate a ridge regressor: ridge
+ridge = Ridge(alpha = 0.5, normalize = True)
+# Perform 5-fold cross-validation: ridge_cv
+ridge_cv = cross_val_score(ridge, X, y, cv = 5)
+# Print the cross-validated scores
+print(ridge_cv)
+
+# ===================== #
+# ==== Ejercicio 24 === #
+# ===================== #
+
+# Convert '?' to NaN
+df[df == '?'] = np.nan
+# Print the number of NaNs
+print(df.isnull().sum())
+# Print shape of original DataFrame
+print("Shape of Original DataFrame: {}".format(df.shape))
+# Drop missing values and print shape of new DataFrame
+df = df.dropna()
+# Print shape of new DataFrame
+print("Shape of DataFrame After Dropping All Rows with Missing Values: {}".format(df.shape))
+
+# ===================== #
+# ==== Ejercicio 25 === #
+# ===================== #
+
+# Import the Imputer module
+from sklearn.preprocessing import Imputer
+from sklearn.svm import SVC
+# Setup the Imputation transformer: imp
+imp = Imputer(missing_values = 'NaN', strategy = 'most_frequent', axis = 0)
+# Instantiate the SVC classifier: clf
+clf = SVC()
+# Setup the pipeline with the required steps: steps
+steps = [('imputation', imp),
+        ('SVM', clf)]
+
+# ===================== #
+# ==== Ejercicio 26 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.preprocessing import Imputer
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+# Setup the pipeline steps: steps
+steps = [('imputation', Imputer(missing_values='NaN', strategy='most_frequent', axis=0)),
+        ('SVM', SVC())]
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+# Fit the pipeline to the train set
+pipeline.fit(X_train, y_train)
+# Predict the labels of the test set
+y_pred = pipeline.predict(X_test)
+# Compute metrics
+print(pipeline.score(X_test, y_test))
+
+# ===================== #
+# ==== Ejercicio 27 === #
+# ===================== #
+
+# Import necessary modules
+from sklearn.preprocessing import Imputer
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+# Setup the pipeline steps: steps
+steps = [('imputation', Imputer(missing_values='NaN', strategy='most_frequent', axis=0)),
+        ('SVM', SVC())]
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+# Fit the pipeline to the train set
+pipeline.fit(X_train, y_train)
+# Predict the labels of the test set
+y_pred = pipeline.predict(X_test)
+# Compute metrics
+print(classification_report(y_test, y_pred))
+
+# ===================== #
+# ==== Ejercicio 28 === #
+# ===================== #
+
+# Import scale
+from sklearn.preprocessing import scale
+# Scale the features: X_scaled
+X_scaled = scale(X)
+# Print the mean and standard deviation of the unscaled features
+print("Mean of Unscaled Features: {}".format(np.mean(X))) 
+print("Standard Deviation of Unscaled Features: {}".format(np.std(X)))
+# Print the mean and standard deviation of the scaled features
+print("Mean of Scaled Features: {}".format(np.mean(X_scaled))) 
+print("Standard Deviation of Scaled Features: {}".format(np.std(X_scaled)))
+
+# ===================== #
+# ==== Ejercicio 29 === #
+# ===================== #
+
+# Import the necessary modules
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+# Setup the pipeline steps: steps
+steps = [('scaler', StandardScaler()),
+        ('knn', KNeighborsClassifier())]
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+# Fit the pipeline to the training set: knn_scaled
+knn_scaled = pipeline.fit(X_train, y_train)
+# Instantiate and fit a k-NN classifier to the unscaled data
+knn_unscaled = KNeighborsClassifier().fit(X_train, y_train)
+# Compute and print metrics
+print('Accuracy with Scaling: {}'.format(knn_scaled.score(X_test, y_test)))
+print('Accuracy without Scaling: {}'.format(knn_unscaled.score(X_test, y_test)))
+
+# ===================== #
+# ==== Ejercicio 30 === #
+# ===================== #
+
+# Setup the pipeline
+steps = [('scaler', StandardScaler()),
+         ('SVM', SVC())]
+pipeline = Pipeline(steps)
+# Specify the hyperparameter space
+parameters = {'SVM__C':[1, 10, 100],
+              'SVM__gamma':[0.1, 0.01]}
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 21)
+# Instantiate the GridSearchCV object: cv
+cv = GridSearchCV(pipeline, param_grid = parameters)
+# Fit to the training set
+cv.fit(X_train, y_train)
+# Predict the labels of the test set: y_pred
+y_pred = cv.predict(X_test)
+# Compute and print metrics
+print("Accuracy: {}".format(cv.score(X_test, y_test)))
+print(classification_report(y_test, y_pred))
+print("Tuned Model Parameters: {}".format(cv.best_params_))
+
+# ===================== #
+# ==== Ejercicio 31 === #
+# ===================== #
+
+# Setup the pipeline steps: steps
+steps = [('imputation', Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)),
+         ('scaler', StandardScaler()),
+         ('elasticnet', ElasticNet())]
+# Create the pipeline: pipeline 
+pipeline = Pipeline(steps)
+# Specify the hyperparameter space
+parameters = {'elasticnet__l1_ratio':np.linspace(0,1,30)}
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
+# Create the GridSearchCV object: gm_cv
+gm_cv = GridSearchCV(pipeline, param_grid = parameters)
+# Fit to the training set
+gm_cv.fit(X_train, y_train)
+# Compute and print the metrics
+r2 = gm_cv.score(X_test, y_test)
+print("Tuned ElasticNet Alpha: {}".format(gm_cv.best_params_))
+print("Tuned ElasticNet R squared: {}".format(r2))
