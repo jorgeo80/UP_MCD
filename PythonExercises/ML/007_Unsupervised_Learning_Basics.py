87aacf895addf94a6bc4afe12d69ff449c27a1a8
Jorge Octavio <jorgeo80@gmail.com>
2020-03-11 21:30:39 -0600

    :dog: Curso Machine Learning

---
+++ PythonExercises/ML/007_Unsupervised_Learning_Basics.py
@@ -0,0 +1,19 @@
+# =========================================================================================== #
+# ========                      Unsupervised Learning: Basics                        ======== #
+# =========================================================================================== #
+
+# ============================== #
+# =====    Ejercicio 1     ===== #
+# ============================== #
+
+# Import plotting class from matplotlib library
+from matplotlib import pyplot as plt
+# Create a scatter plot
+plt.scatter(x, y)
+# Display the scatter plot
+plt.show()
+
+# ============================== #
+# =====    Ejercicio 2     ===== #
+# ============================== #
+
