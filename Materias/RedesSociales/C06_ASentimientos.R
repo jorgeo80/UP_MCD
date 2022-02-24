da3ea6a185a205d1f7a3950e3bdd4ef6f1200e68
JOCR <jorgeo80@gmail.com>
2022-02-23 20:52:47 -0600

    [ADD] Wordclouds

--- Materias/RedesSociales/C06_ASentimientos.R
+++ Materias/RedesSociales/C06_ASentimientos.R
@@ -1,11 +1,3 @@
-# ===================
-# install.packages('wordcloud') 
-# install.packages('tidytext') 
-# install.packages('tokenizers') 
-# install.packages('viridis')
-# install.packages('RColorBrewer')
-# install.packages('viridisLite')
-# ===================
 library(tidyverse)
 library(rtweet)
 library(igraph)
@@ -16,18 +8,45 @@ library(tokenizers)
 library(viridis) 
 
 get_trends('Mexico')$trend
-df_tuits_mrbl <- search_tweets(q="Mr.Blanco's", 
-                                n = 15000, 
-                                type="mixed",
-                                lan='es')
-df_tuits_mrbl
+df_tuits <- search_tweets(q="#PeorQueMegacable", 
+                          n = 5000, 
+                          type="mixed",
+                          lan='es')
+wordcloud(df_tuits$text)
 
-g_tweets <- 
-  tuits %>%
-  filter(retweet_count > 0) %>%
-  select(screen_name, mentions_screen_name) %>%
-  unnest(mentions_screen_name) %>%
-  na.omit() %>%
-  graph_from_data_frame()
+df_tokens <- tibble(message = 1:nrow(df_tuits), 
+                    text = df_tuits$text) %>%
+               tidytext::unnest_tokens(word, text) %>%
+               count(word, sort = TRUE)
 
-g_tweets
+
+wordcloud(words = df_tokens$word, freq = df_tokens$n)
+
+sw <- search_tweets(q = "lang:es",
+                    n = 8000,
+                    encoding = 'UTF-8',
+                    include_rts = FALSE,
+                    retryonratelimit = TRUE,
+                    lang = 'es')
+
+stopwords.spanish <-
+  tokenize_words(sw$text) %>%
+    unlist()  %>%
+    table()
+
+stopwords <- 
+  tibble(word = names(stopwords.spanish), 
+         n = stopwords.spanish) %>% 
+    arrange(-n) %>%
+    head(n = 0.01 * nrow(.))
+
+df_wc <-
+  tibble(message = 1:nrow(df_tuits), 
+         text = df_tuits$text) %>%
+    tidytext::unnest_tokens(word, text) %>%
+    anti_join(stopwords, by = 'word') %>%
+    count(word, sort = TRUE) 
+    #filter(!word %in% c('peorquemegacable'))
+
+wordcloud(words = df_wc$word, freq = df_wc$n)
+
